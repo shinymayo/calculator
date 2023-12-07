28f6c609a9017e4ccc74ec2513c6557688f8c7ec
shinymayo <138251809+shinymayo@users.noreply.github.com>
2023-12-07 15:20:38 +0000

    create basic math functions
    created basic functions to add, subtract, mupltiply and divide.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 addEventListener("load", (event) => {
     console.log("The page is 100% loaded");
 });
+const add = function(a,b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+console.log(add(2,3));
+console.log(subtract(6,1));
+console.log(multiply(5,1));
+console.log(divide(10,2));
