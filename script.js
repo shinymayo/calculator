98c0673163da9f4f2a3bd4f2c67f4a424917b014
shinymayo <138251809+shinymayo@users.noreply.github.com>
2023-12-07 15:45:51 +0000

    operator variable added
    added basic logic to select operator. Added additional classes to btns

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 addEventListener("load", (event) => {
     console.log("The page is 100% loaded");
 });
+
+//selecting buttons
+// const firstNumber = document.querySelector(""); // a
+// const secondNumber = document.querySelector(""); // b
+const operator = document.querySelectorAll(".operator"); // operator
+for(i of operator) {
+    i.addEventListener("click", function() {
+        console.log(this.value);
+    })
+}
+
+//basic math operations
 const add = function(a,b) {
     return a + b;
 }
@@ -16,7 +28,7 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
     return a / b;
 }
-console.log(add(2,3));
-console.log(subtract(6,1));
-console.log(multiply(5,1));
-console.log(divide(10,2));
+// calculator function
+function operate(a, b, operator) { //takes numbers and operator to call one of the math functions
+
+}
