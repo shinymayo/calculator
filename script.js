ea7b1f9d9d609a9e01b56d294359763f46f1a3db
shinymayo <138251809+shinymayo@users.noreply.github.com>
2023-12-17 16:14:56 +0000

    Change calculator's design
    changed color of the calculator, increased button size

--- script.js
+++ script.js
@@ -1,33 +1,6 @@
-addEventListener("load", (event) => {
-    console.log("The page is 100% loaded");
-});
-// to update calculator's display
-const display = document.querySelector(".display"); 
-//selecting buttons
-const firstNumber = document.querySelectorAll(".number");// a
-firstNumber.forEach(function(button) {
-    button.addEventListener("click", function() {
-        console.log(this.value, "first num");
-        display.textContent = this.value;
-        stopPropagation();
-    })
+window.addEventListener("DOMContentLoaded", function() {
+    console.log("Everything is loaded");
 })
- 
-// not working as intended... 
-const secondNumber = document.querySelectorAll(".number"); // b
-secondNumber.forEach(function (button) {
-  button.addEventListener("click", function () {
-    console.log(this.value, "second num");
-  });
-});
-
-const operator = document.querySelectorAll(".operator"); // operator
-for(i of operator) {
-    i.addEventListener("click", function() {
-        console.log(this.value);
-        display.textContent = this.value;
-    })
-}
 
 //basic math operations
 const add = function(a,b) {
