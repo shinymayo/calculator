cb3d137294abd12559b02092b2947daaa7182d50
shinymayo <138251809+shinymayo@users.noreply.github.com>
2023-12-19 15:32:59 +0000

    Reset button functionality added

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ window.addEventListener("DOMContentLoaded", function() {
 })
 
 // define variable you will work with
-const operator = document.querySelectorAll(".operator");
+const operators = document.querySelectorAll(".operator");
 const equal = document.querySelector(".equal");
+const decimal = document.querySelector("#decimal");
 const number = document.querySelectorAll(".number")
 const reset = document.querySelector(".reset");
 const clear = document.querySelector(".clear");
@@ -13,29 +14,46 @@ const calcDisplay = document.querySelector(".display");
 const prevOperand = document.querySelector(".previous-number");
 const currOperand = document.querySelector(".current-number");
 
+let previousNumber = "";
+let currentNumber = "";
+let operator = "";
+
 //function to clear
 clear.addEventListener("click", function() {
     currOperand.textContent = currOperand.textContent.toString().slice(0, -1);
 })
-
 //function to reset
-
+reset.addEventListener("click", function() {
+    previousNumber = "";
+    currentNumber = "";
+    operator = "";
+    prevOperand.textContent = currentNumber;
+    currOperand.textContent = currentNumber;
+})
 
 //to get numbers for 1st operand
-number.forEach(function(button) {
+number.forEach((button) => {
     button.addEventListener("click", function() {
-        currOperand.textContent += this.value;
-        //should i do if function here to get the second number?
+            currOperand.textContent += this.value;
+            currentNumber = currOperand.textContent;
     })
 })
 
 // to get the operator 
-for(oper of operator) {
-    oper.addEventListener("click", function() {
-        currOperand.textContent = this.value; 
+operators.forEach((button) => {
+    button.addEventListener("click", function() {
+        currOperand.textContent = this.value;
+        handleOperator(button);
     })
-}
+})
 
+ function handleOperator(button) {
+    operator = button;
+    previousNumber = currentNumber;
+    currentNumber = "";
+    prevOperand.append(previousNumber);
+
+ }
 // to get 2nd operator
 //??
 
